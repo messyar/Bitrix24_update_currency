b1270bb30f4f39344c0ce1e2926a99669a426f30
Andrey Yaroslavtsev <messyar@gmail.com>
2021-10-07 19:29:42 +0500

    Finish update and add new currency, if it's sets in config file. TODO tests

--- currency_actualizer.py
+++ currency_actualizer.py
@@ -5,23 +5,34 @@ from datetime import datetime
 from os import environ
 
 try:
-    from currency_actualizer_config import CURRENCY_TO_UPDATE
+    from currency_actualizer_config import CURRENCY_TO_UPDATE, ADD_NEW_CURRENCY, LOCALES
 except ImportError as import_error:
-    print('Config file import error. Possible, you need to create currency_actualizer_config.py'
-          'with list constant, named CURRENCY_TO_UPDATE'
-          'For example CURRENCY_TO_UPDATE=["USD", "EUR"]')
+    print("""Config file import error. Possible, you need to create currency_actualizer_config.py
+          with list constant, named CURRENCY_TO_UPDATE
+          For example \n
+          CURRENCY_TO_UPDATE=["USD", "EUR"]\n
+          ADD_NEW_CURRENCY=True\n
+          LOCALES = [{
+                        'ru':
+                                {
+                                    'DEC_POINT': '.',
+                                    'DECIMALS': 2,
+                                    'HIDE_ZERO': 'Y'
+                                },
+          }]""")
 
 
 def parse_exchange_rates(currency_to_update: list) -> dict:
     """
     This function parses the exchange rates from the Central bank of Russia website
-    :return: result = {'USD': '0', 'EUR': '0', 'KZT': '0', 'PLN': '0'}
+    :return: list of dicts with exchange information from cbr.ru
     """
 
-    result = {}
+    result = []
     day = datetime.now().day
     month = datetime.now().month
     year = datetime.now().year
+
     if int(day) < 10:
         day = '0%s' % day
 
@@ -51,8 +62,16 @@ def parse_exchange_rates(currency_to_update: list) -> dict:
         currency_list = structure.findall("./Valute")
         for curr_currency in currency_list:
             curr_currency_text = curr_currency.find('CharCode').text
+
             if curr_currency_text in currency_to_update:
-                result[curr_currency_text] = curr_currency.find('Value').text.replace(',', '.')
+
+                currency_info = {}
+                for curr_tag in curr_currency:
+                    if curr_tag.tag == 'Value':
+                        currency_info[curr_tag.tag] = curr_tag.text.replace(',', '.')
+                    else:
+                        currency_info[curr_tag.tag] = curr_tag.text
+                result.append(currency_info)
 
     except ET.ParseError as PE:
         print(PE)
@@ -74,12 +93,14 @@ def update_currency() -> None:
         return
 
     bx24 = Bitrix24(url_to_api)
+
     try:
         base_currency = bx24.callMethod('crm.currency.base.get')
+
     except BitrixError as message:
         print(message)
 
-    if not base_currency == 'RUB':
+    if base_currency != 'RUB':
         print('Base currency not RUB, this script work only if base currency in Bitrix24 is RUB')
         return
 
@@ -88,15 +109,50 @@ def update_currency() -> None:
         print('Please, specify the currencies to update the exchange rate in the currency_actualizer_file.py'
               ' as a list constant named CURRENCY_TO_UPDATE. For example CURRENCY_TO_UPDATE=["USD", "EUR"]')
         return
+
     currency_pairs = parse_exchange_rates(currency_to_update)
+    if not currency_pairs:
+        print("Error! Exchange rate parse from cbr.ru failed.")
+        return
 
-    try:
-        for key, value in currency_pairs.items():
-            print(key, value)
-            bx24.callMethod('crm.currency.update', id=key, fields={'AMOUNT_CNT': 1, 'AMOUNT': value})
+    not_added_currency = []
+    for curr_element in currency_pairs:
+        try:
+            bx24.callMethod('crm.currency.update', id=curr_element['CharCode'],
+                            fields={'AMOUNT_CNT': curr_element['Nominal'],
+                                    'AMOUNT': curr_element['Value']})
 
-    except BitrixError as message:
-        print(message)
+        except BitrixError as message:
+
+            if message.args[0] == 'Currency is not found':
+                if ADD_NEW_CURRENCY:
+                    print('Missed currency ', curr_element['CharCode'])
+
+                    fields = {'CURRENCY': curr_element['CharCode'], 'AMOUNT_CNT': curr_element['Nominal'],
+                              'AMOUNT': curr_element['Value'], 'SORT': 1000}
+                    locale = {}
+
+                    for key, value in LOCALES.items():
+                        locale[key] = value
+
+                        if key == 'ru':
+                            locale[key]['FORMAT_STRING'] = '%23' + ' ' + curr_element['Name'].split()[1]
+                            locale[key]['FULL_NAME'] = curr_element['Name']
+                            locale[key]['THOUSANDS_VARIANT'] = "C"
+                            locale[key]['THOUSANDS_SEP'] = "'"
+
+                    fields['LANG'] = locale
+                    bx24.callMethod('crm.currency.add', fields=fields)
+
+                    print('Currency {} was added'.format(curr_element['CharCode']))
+                else:
+                    not_added_currency.append(curr_element['CharCode'])
+            else:
+                print(message)
+
+    if not ADD_NEW_CURRENCY:
+        print("""In config file constant ADD_NEW_CURRENCY={}. Because of that currency {} just 
+        pass and don't be added in yours Bitrix24""".format(ADD_NEW_CURRENCY, not_added_currency))
 
 
 if __name__ == '__main__':
--- currency_actualizer_config.py
+++ currency_actualizer_config.py
@@ -1,1 +1,20 @@
+"""
+List of currency need to update
+"""
 CURRENCY_TO_UPDATE = ['USD', 'EUR', 'KZT', 'PLN']
+""" 
+If ADD_NEW_CURRENCY = True currency, that not found in Bitrix24 will be added
+else it'll be ignoring
+"""
+ADD_NEW_CURRENCY = True
+"""
+This constant sets format for currency locales, for now only 'ru' supports
+"""
+LOCALES = {
+    'ru':
+        {
+            'DEC_POINT': '.',
+            'DECIMALS': 2,
+            'HIDE_ZERO': "Y"
+        },
+}
